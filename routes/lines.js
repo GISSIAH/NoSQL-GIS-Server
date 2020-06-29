292af8ae5c6fcb066e04a13f86c2295ffd857e76
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2020-06-29 13:27:14 +0200

    Update lines.js

--- routes/lines.js
+++ routes/lines.js
@@ -4,21 +4,49 @@ const {ln} = require('../models/models.js');
 api.get('/all',(req,res,next)=>{
     console.log('This is ALL');
     ln.find({}).then((fts)=>{
-        
-        res.send(fts);
+        var coll ={
+            "type": "FeatureCollection",
+                "name": "All Lines",
+                    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                "features":fts
+            }
+            res.send(coll);
     })
 });
 
 api.get('/all/:layer',(req,res,next)=>{
     ln.find({Layername:req.params.layer}).then((fts)=>{
-        res.send(fts);
+        var coll ={
+            "type": "FeatureCollection",
+                "name":req.params.layer ,
+                    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                "features":fts
+            }
+            res.send(coll);
     });
 });
 
 api.get('/all/:name',(req,res,next)=>{
     ln.find({name:req.params.name}).then((pt)=>{
-        res.send(pt);
+        var coll ={
+            "type": "FeatureCollection",
+                "name":req.params.name,
+                    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                "features":fts
+            }
+            res.send(coll);
     });
 });
+
+api.post('/entry',(req,res,next)=>{
+    ln.create(req.body).
+        then(function(li){
+            res.send(li);
+    }).catch(next)
+})
+
+
+
+
 
 module.exports = api;
